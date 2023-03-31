afc330e6a3c6e4987a72b03aef9e041f9934bef7
Niky-Niky <nik333nik888@gmail.com>
2023-03-31 15:59:58 +0200

    First commit
    First commit of practicing querySelector

---
+++ index.js
@@ -0,0 +1,8 @@
+// const magicBtn = document.querySelector('.js-magic-btn');
+const navEl = document.querySelector('.site-nav');
+console.log(navEl)
+const tazEl = document.querySelector('.dumb-taz');
+console.log(tazEl)
+tazEl.src = 'Looney.png'
+tazEl.alt = 'Hello, you seem to be dumb (sometimes)'
+console.log(tazEl.alt);
